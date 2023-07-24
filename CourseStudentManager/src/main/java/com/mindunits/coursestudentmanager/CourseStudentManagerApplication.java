27057fb6bb4d322caf7f25022db0c5245d108a23
acosta604 <paulaacosta604@gmail.com>
2023-07-24 17:52:01 -0500

    Project creation with Spring initializr

---
+++ CourseStudentManager/src/main/java/com/mindunits/coursestudentmanager/CourseStudentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mindunits.coursestudentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseStudentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseStudentManagerApplication.class, args);
+	}
+
+}
---
+++ CourseStudentManager/src/test/java/com/mindunits/coursestudentmanager/CourseStudentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindunits.coursestudentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseStudentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
