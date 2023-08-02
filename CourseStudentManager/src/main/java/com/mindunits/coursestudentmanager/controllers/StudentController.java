dc813cbc1aa36e3204afa168c5d5141051d54b66
Miguel Mallqui <132778229+yaspercode@users.noreply.github.com>
2023-08-02 00:12:10 -0500

    feature/ADCYE-5/Inscripcion-y-cancelacion-de-cursos

--- CourseStudentManager/src/main/java/com/mindunits/coursestudentmanager/controllers/StudentController.java
+++ CourseStudentManager/src/main/java/com/mindunits/coursestudentmanager/controllers/StudentController.java
@@ -1,13 +1,16 @@
 package com.mindunits.coursestudentmanager.controllers;
 
 
+import com.mindunits.coursestudentmanager.models.Course;
+import com.mindunits.coursestudentmanager.models.Enrollment;
 import com.mindunits.coursestudentmanager.models.Student;
 import com.mindunits.coursestudentmanager.repository.StudentRepository;
 import com.mindunits.coursestudentmanager.validators.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -15,26 +18,41 @@ public class StudentController {
     EmailValidator emailValidator = new EmailValidator();
 
     @Autowired
-    StudentRepository repositorioDeEstudiantes;
+    StudentRepository studentRepository;
 
 
     @PostMapping("/api/student")
-    public String registrarse(@RequestBody Student student){
+    public String register(@RequestBody Student student){
 
         String studentMail = student.getEmail();
-        String studentName = student.getStudent_name();
+        String studentName = student.getName();
         String studentPhone = student.getPhone();
 
         if(emailValidator.esValido(studentMail) == false){
-            return "No es un mail válido";
+            return "Not a valid email.";
         }
 
-        repositorioDeEstudiantes.guardarEstudiante(studentName, studentMail, studentPhone);
+        studentRepository.saveStudent(studentName, studentMail, studentPhone);
 
-        return "Estudiante agregado a base de datos.";
+        return "Student added to the database.";
 
     }
-
-
+    @DeleteMapping("/api/student/{id}")
+    public void deleteCourse(@PathVariable("id") Long id) {
+        studentRepository.courseCancellation(id);
+    }
+    @PostMapping("/api/registerstudentcourses")
+    public String registerCourse(@RequestBody Enrollment enrollment) {
+        Long idStudent = enrollment.getStudent().getId();
+        Long idCourse = enrollment.getCourse().getId();
+        Date date = enrollment.getDate();
+        String status = enrollment.getStatus();
+        studentRepository.courseRegistration(idStudent, idCourse, date, status);
+        return "Course successfully enrolled.";
+    }
+    @GetMapping("/api/student")
+    public List<Enrollment> getAll() {
+        return studentRepository.getAllEnrollments();
+    }
 
 }
--- CourseStudentManager/src/main/java/com/mindunits/coursestudentmanager/repository/StudentRepository.java
+++ CourseStudentManager/src/main/java/com/mindunits/coursestudentmanager/repository/StudentRepository.java
@@ -1,32 +1,62 @@
 package com.mindunits.coursestudentmanager.repository;
 
 
+import com.mindunits.coursestudentmanager.models.Course;
+import com.mindunits.coursestudentmanager.models.Enrollment;
+import com.mindunits.coursestudentmanager.models.Student;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public class StudentRepository {
 
 
     @PersistenceContext
-    EntityManager baseDeDatos;
+    EntityManager database;
 
 
     @Transactional
-    public void guardarEstudiante(String nameStudent, String emailStudent, String phoneStudent){
+    public void saveStudent(String nameStudent, String emailStudent, String phoneStudent){
 
-        String consultaSql = "INSERT INTO Student (student_name, email, phone) VALUES (:nameParametro, :emailParametro, :phoneParametro)";
+        String saveStudentSql = "INSERT INTO Student (student_name, email, phone) VALUES (:nameParametro, :emailParametro, :phoneParametro)";
 
-        baseDeDatos.createQuery(consultaSql)
+        database.createQuery(saveStudentSql)
                 .setParameter("nameParametro", nameStudent)
                 .setParameter("emailParametro", emailStudent)
                 .setParameter("phoneParametro", phoneStudent)
                 .executeUpdate();
     }
 
+    @Transactional
+    public void courseRegistration(Long idStudent, Long idCourse, Date date, String status) {
+        String courseRegistrationSql = "INSERT INTO Enrollment (id_student, id_course, enrollment_date, status) " +
+                "VALUES (:paramIdStudent, :paramIdCourse, :paramDate, :paramStatus)";
+        database.createNativeQuery(courseRegistrationSql)
+                .setParameter("paramIdStudent", idStudent)
+                .setParameter("paramIdCourse", idCourse)
+                .setParameter("paramDate", date)
+                .setParameter("paramStatus", status)
+                .executeUpdate();
+    }
+    @Transactional
+    public void courseCancellation(Long id){
+        String courseCancellationSql = "DELETE FROM Enrollment WHERE id = :paramId";
+        database.createQuery(courseCancellationSql)
+                .setParameter("paramId", id)
+                .executeUpdate();
+    }
 
+    @Transactional
+    public List<Enrollment> getAllEnrollments(){
+        String selectAllSql = "FROM Enrollment";
+        return database.createQuery(selectAllSql, Enrollment.class)
+                .getResultList();
+    }
 
 
     //INSERT INTO `coursestudentmanager`.`student`
